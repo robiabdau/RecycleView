2a3ded2e7c32b335948307e9c807b29ea2584732
robiabdau <abdaurobi@gmail.com>
2022-06-22 17:48:10 +0700

    Recycleview APP

--- app/src/main/java/com/robiabdau/recycleview/MainActivity.kt
+++ app/src/main/java/com/robiabdau/recycleview/MainActivity.kt
@@ -4,7 +4,9 @@ import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import com.robiabdau.recycleview.adapter.AdapterTeamBola
 import com.robiabdau.recycleview.databinding.ActivityMainBinding
 import com.robiabdau.recycleview.model.Pemain
@@ -27,7 +29,33 @@ class MainActivity : AppCompatActivity() {
 
         binding.list.adapter = AdapterTeamBola(this,listPemain,object : AdapterTeamBola.OnClickListener {
             override fun detailData(item: Pemain?) {
-                TODO("Not yet implemented")
+                Dialog(this@MainActivity).apply {
+                    requestWindowFeature(Window.FEATURE_NO_TITLE)
+                    setCancelable(true)
+                    setContentView(R.layout.detail_data_pemain)
+
+                    val image = this.findViewById<ImageView>(R.id.image_pemain)
+                    val nama = this.findViewById<TextView>(R.id.txt_namapemain)
+
+                    val posisi = this.findViewById<TextView>(R.id.txt_posisi)
+                    val tinggi = this.findViewById<TextView>(R.id.txt_tinggibadan)
+                    val tempatlahir = this.findViewById<TextView>(R.id.txt_tempatlahir)
+                    val tgllahir = this.findViewById<TextView>(R.id.txt_tanggallahir)
+                    val btn = this.findViewById<Button>(R.id.btnClose)
+
+                    image.setImageResource(item?.foto ?:0)
+                    nama.text = "${item?.nama}"
+                    posisi.text = "${item?.posisi}"
+                    tinggi.text = "${item?.tinggi}"
+                    tempatlahir.text = "${item?.tempatlahir}"
+                    tgllahir.text = "${item?.tgllahir}"
+
+                    btn.setOnClickListener {
+                        this.dismiss()
+                    }
+
+
+                }.show()
             }
 
         })
